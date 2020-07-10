fd6d5c9bfd51c399d951fee976260ce427f251d6
Vitor Ivan D'Angelo <metroid.p2p@gmail.com>
2020-07-10 08:06:30 -0300

    Form complete

--- burger-builder/src/components/UI/Input/Input.js
+++ burger-builder/src/components/UI/Input/Input.js
@@ -4,12 +4,17 @@ import classes from "./Input.css";
 
 const input = (props) => {
   let inputElement = null;
+  const inputClasses = [classes.InputElement];
+
+  if (props.invalid && props.shouldValidate && props.touched) {
+    inputClasses.push(classes.Invalid);
+  }
 
   switch (props.elementType) {
     case "input":
       inputElement = (
         <input
-          className={classes.InputElement}
+          className={inputClasses.join(" ")}
           {...props.elementConfig}
           value={props.value}
           onChange={props.changed}
@@ -19,7 +24,7 @@ const input = (props) => {
     case "textarea":
       inputElement = (
         <textarea
-          className={classes.InputElement}
+          className={inputClasses.join(" ")}
           {...props.elementConfig}
           value={props.value}
           onChange={props.changed}
@@ -28,8 +33,11 @@ const input = (props) => {
       break;
     case "select":
       inputElement = (
-        <select className={classes.InputElement} value={props.value}
-        onChange={props.changed}>
+        <select
+          className={inputClasses.join(" ")}
+          value={props.value}
+          onChange={props.changed}
+        >
           {props.elementConfig.options.map((option) => (
             <option key={option.value} value={option.value}>
               {option.displayValue}
@@ -41,7 +49,7 @@ const input = (props) => {
     default:
       inputElement = (
         <input
-          className={classes.InputElement}
+          className={inputClasses.join(" ")}
           {...props.elementConfig}
           value={props.value}
           onChange={props.changed}
@@ -51,7 +59,7 @@ const input = (props) => {
 
   return (
     <div className={classes.Input}>
-      <label className={classes.label}>{props.label}</label>
+      <label className={classes.Label}>{props.label}</label>
       {inputElement}
     </div>
   );
